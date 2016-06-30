5fee726882123cd20f1fa7b826d48cb2f8924c6e
cjwit <chris.witulski@gmail.com>
2016-06-30 09:28:04 -0400

    remove contour

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name = 'contourviz',
     version = '0.1',
     description = 'A package that charts musical contours into a web-based interactive using music21 and D3.js.',
-    url = 'https://github.com/cjwit/contour',
+    url = 'https://github.com/cjwit/contourviz',
     author = 'Christopher Witulski',
     author_email = 'chris.witulski@gmail.com',
     license = 'BSD',
-    packages = ['contour'],
+    packages = ['contourviz'],
     zip_safe = False)
