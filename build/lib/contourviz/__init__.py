a77abb3d3d808d7b9480e8f658d33e11f2d3c3d9
cjwit <chris.witulski@gmail.com>
2016-06-30 09:50:43 -0400

    uninstall contour build

---
+++ build/lib/contourviz/__init__.py
@@ -0,0 +1,1 @@
+from .contourviz import joke
---
+++ build/lib/contourviz/contourviz.py
@@ -0,0 +1,6 @@
+from markdown import markdown
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer?'
+            u'Ja! ... '
+            u'**Beiherhund** das Oder die Flipperwaldt gersput.')
