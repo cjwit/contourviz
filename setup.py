c4ccb3196b2d8c3673a0b312a80724d0d2011d8e
cjwit <chris.witulski@gmail.com>
2016-06-30 09:02:56 -0400

    add setup, organize structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'contour',
+    version = '0.1',
+    description = 'A package that charts musical contours into a web-based interactive using music21 and D3.js.',
+    url = 'https://github.com/cjwit/contour',
+    author = 'Christopher Witulski',
+    author_email = 'chris.witulski@gmail.com',
+    license = 'BSD',
+    packages = ['contour'],
+    zip_safe = False)
